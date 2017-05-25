234755846e3c710b276dcb8830d0ff54f96d9927
goutham <gouthamchintu.sgr@gmail.com>
2017-05-25 21:58:45 +0530

    Modified templates to display posts from database

--- myblog/settings.py
+++ myblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_k6mggzo&-^#o_-s*l5%v6bwiug8%%#ltr5x^a#ixqom(mrrv#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','http://gouthamreddy.pythonanywhere.com/']
 
 
 # Application definition
